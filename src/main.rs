e522f7a2397b55b374b8ac5edb0d2bcd59611304
Juxhin Dyrmishi Brigjaj <juxhinbox@gmail.com>
2021-01-26 23:54:24 +0100

    More fun trying to get encoding to work

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,24 @@
 fn main() {
-    let mut raw: u64 = 0b1100101101010011010101111010001;
+    let mut raw: u64 = 0b10011010;
     println!("    Raw: {:064b}", raw);
 
     let mut code = encode(&mut raw);
-    //let block = decode(&mut code);
+    let block = decode(&mut code);
 
     println!("Encoded: {:064b}", code);
-    //println!("Decoded: {:064b}", block);
+    println!("Decoded: {:064b}", block);
 }
 
-#[inline(always)]
 fn encode(block: &mut u64) -> u64 {
     let len_power = (2..).find(|&r| 2u32.pow(r) - r - 1 >= 32).unwrap();
     let len = 2usize.pow(len_power) - 1;
 
     let mut code = 0u64;
 
-    for i in 1..len {
+    for i in 1..len + 1 {
         // Check if `i` is not a power of 2
         if (i & (i - 1)) != 0 {
-            code |= (0b1 << i - 1) & *block as u64;
+            code |= (0b1 << i) & *block as u64;
         } else {
             *block <<= 1;
         }
@@ -27,47 +26,60 @@ fn encode(block: &mut u64) -> u64 {
 
     for i in 0..len_power {
         if parity(&code, i) {
-            code |= 1u64 << (2usize.pow(i) - 1);
+            code |= 0b1 << (2usize.pow(i) - 1);
         } else {
-            let mask = 1u64 << (2usize.pow(i) - 1);
-            code &= !mask;
+            let mut mask = u64::MAX;
+            mask ^= 0b1 << (2usize.pow(i) - 1);
+            println!("code: {:064b}, {}", code, 2usize.pow(i) - 1);
+            code &= mask;
+            println!("code: {:064b}\n\n", code);
         }
     }
 
     code
 }
 
-//#[inline(always)]
-//fn decode(code: &mut u64) -> u64 {
-//    let len_power = ((64 + 1) as f64).sqrt().round() as u64;
-//    let mut flipped_bit = -1i32;
-//
-//    while (0..len_power).any(|i| parity(&code, i as u32) == 1u64) {
-//        if flipped_bit != -1 {
-//            *code ^= 1u64 << flipped_bit;
-//        }
-//
-//        flipped_bit += 1;
-//        *code ^= 1u64 << flipped_bit;
-//    }
-//
-//    *code
-//}
+fn decode(code: &mut u64) -> u64 {
+    let len_power = 7;
+    let mut flipped_bit = -1i32;
+
+    let mut decoded = 0u64;
+
+    for i in 0..len_power {
+        if parity(&code, i) {
+            if flipped_bit != -1 {
+                *code ^= 0b1 << flipped_bit;
+            }
+
+            flipped_bit += 1;
+            *code ^= 0b1 << flipped_bit;
+        }
+    }
+
+    for i in 0..64 {
+        if (i & (i + 1)) != 0 {
+            decoded |= (0b1 << i) & *code;
+        }
+    }
+
+    decoded
+}
 
 // TODO(jdb): Optimize this, remove internal bools and flags
 fn parity(code: &u64, i: u32) -> bool {
-    let bi = 2u32.pow(i) - 1;
+    let bi = (0b1 << i) - 1;
     let (mut parity, mut ignore, mut counter) = (false, false, 0);
     for j in bi..64 {
-        let mask = 0b1 << j;
-        if !ignore && (code & mask) == mask {
+        if !ignore && (code & 0b1 << j) != 0b0 {
             parity = !parity;
         }
+
         counter += 1;
-        if counter >= 2u32.pow(i) {
+        if counter > 0b1 << i {
             ignore = !ignore;
             counter = 0;
         }
     }
+
     parity
 }
