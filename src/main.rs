0395c3cc0b54db7628d4fd6991557a55978abfa3
Juxhin Dyrmishi Brigjaj <juxhinbox@gmail.com>
2021-02-19 23:45:39 +0100

    Fix little-endian encoding

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 fn main() {
-    let mut raw: u64 = 0b10011010;
-    println!("    Raw: {:064b}", raw);
+    let mut raw: u64 = 0b1001;
+    println!("    Raw  (2): {:064b}", raw);
+    println!("    Raw (10): {}", raw);
 
-    let mut code = encode(&mut raw);
-    let block = decode(&mut code);
-
+    let code = encode(&mut raw);
     println!("Encoded: {:064b}", code);
-    println!("Decoded: {:064b}", block);
+
+    //let block = decode(&mut code);
+    //println!("Decoded: {:064b}", block);
 }
 
 fn encode(block: &mut u64) -> u64 {
@@ -18,68 +19,66 @@ fn encode(block: &mut u64) -> u64 {
     for i in 1..len + 1 {
         // Check if `i` is not a power of 2
         if (i & (i - 1)) != 0 {
-            code |= (0b1 << i) & *block as u64;
+            code |= (0b1 << i - 1) & *block as u64;
         } else {
             *block <<= 1;
         }
     }
 
+    let mut encoded = code;
     for i in 0..len_power {
-        if parity(&code, i) {
-            code |= 0b1 << (2usize.pow(i) - 1);
-        } else {
-            let mut mask = u64::MAX;
-            mask ^= 0b1 << (2usize.pow(i) - 1);
-            println!("code: {:064b}, {}", code, 2usize.pow(i) - 1);
-            code &= mask;
-            println!("code: {:064b}\n\n", code);
+        // If the parity check if odd, set the bit to 1
+        // otherwise move on.
+        if !parity(&code, i) {
+            encoded |= 0b1 << (2usize.pow(i) - 1);
         }
     }
 
-    code
+    encoded
 }
 
-fn decode(code: &mut u64) -> u64 {
-    let len_power = 7;
-    let mut flipped_bit = -1i32;
+//fn decode(code: &mut u64) -> u64 {
+//    let len_power = 7;
+//    let mut flipped_bit = -1i32;
+//
+//    let mut decoded = 0u64;
+//
+//    for i in 0..len_power {
+//        if parity(&code, i) {
+//            if flipped_bit != -1 {
+//                *code ^= 0b1 << flipped_bit;
+//            }
+//
+//            flipped_bit += 1;
+//            *code ^= 0b1 << flipped_bit;
+//        }
+//    }
+//
+//    for i in 0..64 {
+//        if (i & (i + 1)) != 0 {
+//            decoded |= (0b1 << i) & *code;
+//        }
+//    }
+//
+//    decoded
+//}
 
-    let mut decoded = 0u64;
-
-    for i in 0..len_power {
-        if parity(&code, i) {
-            if flipped_bit != -1 {
-                *code ^= 0b1 << flipped_bit;
-            }
-
-            flipped_bit += 1;
-            *code ^= 0b1 << flipped_bit;
-        }
-    }
-
-    for i in 0..64 {
-        if (i & (i + 1)) != 0 {
-            decoded |= (0b1 << i) & *code;
-        }
-    }
-
-    decoded
-}
-
-// TODO(jdb): Optimize this, remove internal bools and flags
 fn parity(code: &u64, i: u32) -> bool {
     let bi = (0b1 << i) - 1;
-    let (mut parity, mut ignore, mut counter) = (false, false, 0);
+    let (mut parity, mut ignore, mut counter) = (true, false, 0);
     for j in bi..64 {
         if !ignore && (code & 0b1 << j) != 0b0 {
             parity = !parity;
         }
 
         counter += 1;
-        if counter > 0b1 << i {
+        if counter >= 0b1 << i {
             ignore = !ignore;
             counter = 0;
         }
     }
 
+    // true if even
+    // false if odd
     parity
 }
