a87a8a9a6c666726659f5ffd7b7d0b8c94cd3b9a
Juxhin Dyrmishi Brigjaj <juxhinbox@gmail.com>
2021-03-15 22:22:03 +0100

    Fix encoder

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
 fn main() {
     let mut raw: u64 = 0b1001;
     println!("    Raw  (2): {:064b}", raw);
-    println!("    Raw (10): {}", raw);
 
-    let code = encode(&mut raw);
+    let mut code = encode(&mut raw);
     println!("Encoded: {:064b}", code);
 
-    //let block = decode(&mut code);
-    //println!("Decoded: {:064b}", block);
+    let block = decode(&mut code);
+    println!("Decoded: {:064b}", block);
 }
 
 fn encode(block: &mut u64) -> u64 {
     let len_power = (2..).find(|&r| 2u32.pow(r) - r - 1 >= 32).unwrap();
-    let len = 2usize.pow(len_power) - 1;
+    let len = 2usize.pow(len_power);
 
     let mut code = 0u64;
 
-    for i in 1..len + 1 {
+    for i in 1..len {
         // Check if `i` is not a power of 2
         if (i & (i - 1)) != 0 {
             code |= (0b1 << i - 1) & *block as u64;
@@ -27,41 +26,49 @@ fn encode(block: &mut u64) -> u64 {
 
     let mut encoded = code;
     for i in 0..len_power {
-        // If the parity check if odd, set the bit to 1
-        // otherwise move on.
+        // If the parity check is odd, set the bit to 1 otherwise move on.
         if !parity(&code, i) {
             encoded |= 0b1 << (2usize.pow(i) - 1);
         }
     }
 
+    // Set global parity bit P0
+    // This needs to be done last as it's a global parity
+    // check on the final result..
+    //
+    // TODO(JDB): See how to streamline this
+    //if parity(&encoded, 0) {
+    //    encoded |= 0b1
+    //}
+
     encoded
 }
 
-//fn decode(code: &mut u64) -> u64 {
-//    let len_power = 7;
-//    let mut flipped_bit = -1i32;
-//
-//    let mut decoded = 0u64;
-//
-//    for i in 0..len_power {
-//        if parity(&code, i) {
-//            if flipped_bit != -1 {
-//                *code ^= 0b1 << flipped_bit;
-//            }
-//
-//            flipped_bit += 1;
-//            *code ^= 0b1 << flipped_bit;
-//        }
-//    }
-//
-//    for i in 0..64 {
-//        if (i & (i + 1)) != 0 {
-//            decoded |= (0b1 << i) & *code;
-//        }
-//    }
-//
-//    decoded
-//}
+fn decode(code: &mut u64) -> u64 {
+    let len_power = 7;
+    let mut flipped_bit = -1i32;
+
+    let mut decoded = 0u64;
+
+    for i in 0..len_power {
+        if parity(&code, i) {
+            if flipped_bit != -1 {
+                *code ^= 0b1 << flipped_bit;
+            }
+
+            flipped_bit += 1;
+            *code ^= 0b1 << flipped_bit;
+        }
+    }
+
+    for i in 0..64 {
+        if (i & (i + 1)) != 0 {
+            decoded |= (0b1 << i) & *code;
+        }
+    }
+
+    decoded
+}
 
 fn parity(code: &u64, i: u32) -> bool {
     let bi = (0b1 << i) - 1;
