c0854e52093e64ea8eca911d0fe66eb612ddc314
Juxhin Dyrmishi Brigjaj <juxhinbox@gmail.com>
2021-04-04 11:10:10 +0200

    Randomise benchmark inputs

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,9 +1,20 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use hamming_code_simd::hamming::{decode, encode};
+use rand::Rng;
 
 pub fn benchmark_encode(c: &mut Criterion) {
-    c.bench_function("hamming encode", |b| b.iter(|| encode(black_box(&mut 20))));
+    let mut input = rand::thread_rng().gen::<u64>();
+    c.bench_function("hamming encode", |b| {
+        b.iter(|| encode(black_box(&mut input)))
+    });
 }
 
-criterion_group!(benches, benchmark_encode);
+pub fn benchmark_decode(c: &mut Criterion) {
+    let mut input = rand::thread_rng().gen::<u64>();
+    c.bench_function("hamming decode", |b| {
+        b.iter(|| decode(black_box(&mut input)))
+    });
+}
+
+criterion_group!(benches, benchmark_encode, benchmark_decode);
 criterion_main!(benches);
